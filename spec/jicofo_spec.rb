5fb77fdcc6016c16e976d5455fb8d8a67f6e07a3
Conor Schaefer <conor.schaefer@gmail.com>
2016-03-01 12:34:45 -0800

    Starts jicofo tests

---
+++ spec/jicofo_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe file('/etc/jitsi/jicofo/config') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its('mode') { should eq '644' }
+  its('content') { should match(/^JICOFO_HOST=localhost$/) }
+  its('content') { should match(/^JICOFO_PORT=5347$/) }
+  its('content') { should match(/^JICOFO_SECRET=\w{8,}$/) }
+  its('content') { should match(/^JICOFO_AUTH_PASSWORD=\w{8,}$/) }
+  its('content') { should match(/^JICOFO_AUTH_USER=focus$/) }
+end
+
+
