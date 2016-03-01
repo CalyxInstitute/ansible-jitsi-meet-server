3d9a121eca81381d52c7cc7b6e72e6fdb4539e2b
Conor Schaefer <conor.schaefer@gmail.com>
2016-03-01 14:00:41 -0800

    Tougher serverspec tests for prosody config
    Parsing lua with sed is maddening.

--- spec/prosody_spec.rb
+++ spec/prosody_spec.rb
@@ -8,6 +8,21 @@ describe file('/etc/prosody/conf.avail/localhost.cfg.lua') do
 
   its('content') do
     should contain(
+      'VirtualHost "localhost"').before(
+        'authentication = "anonymous"')
+  end
+
+  wanted_enabled_modules = %w(bosh pubsub ping)
+  wanted_enabled_modules.each do |enabled_module|
+    its('content') do
+      should contain(
+        enabled_module.to_s).from(
+          /^\s+modules_enabled = \{/).to(
+            /^\s+\}$/)
+    end
+  end
+  its('content') do
+    should contain(
       'VirtualHost "auth.localhost"').before(
         'authentication = "internal_plain"')
   end
