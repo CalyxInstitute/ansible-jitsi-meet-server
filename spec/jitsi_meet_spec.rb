4f59fd0cc96f8d4591bab2b30ff5ee32ae9e08fc
Conor Schaefer <conor.schaefer@gmail.com>
2016-03-01 13:04:17 -0800

    Sets server name to localhost by default, FQDN if SSL
    Lines up with how the Debian package configures the services. Need to
    add more conditional logic on the config removal, but otherwise works
    pretty well.

--- spec/jitsi_meet_spec.rb
+++ spec/jitsi_meet_spec.rb
@@ -3,3 +3,22 @@ require 'spec_helper'
 describe package('jitsi-meet') do
   it { should be_installed }
 end
+
+describe package('default-jre-headless') do
+  it { should be_installed }
+end
+
+describe command('echo "get jitsi-meet/jvb-hostname" | \
+                  debconf-communicate') do
+  its('stdout') { should eq "0 localhost\n" }
+end
+
+describe command('echo "get jitsi-meet/jvb-serve" | \
+                  debconf-communicate') do
+  its('stdout') { should eq "0 false\n" }
+end
+
+describe command('echo "get jitsi-meet-prosody/jvb-hostname" | \
+                  debconf-communicate') do
+  its('stdout') { should eq "0 localhost\n" }
+end
