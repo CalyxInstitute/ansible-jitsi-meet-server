db1bebd3e9a18622f0ac6727c4ca35e2ecd19ae1
Conor Schaefer <conor@freedom.press>
2016-11-01 20:40:45 -0700

    Updates apt key management to clean up old keys
    The GPG key fingerprint for the Jitsi stable repo was incorrect.
    Further testing required to determine whether stable/unstable require
    different signature verifications.

--- spec/apt_repo_spec.rb
+++ spec/apt_repo_spec.rb
@@ -19,3 +19,22 @@ describe file('/etc/apt/sources.list.d/jitsi_meet.list') do
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
 end
+
+describe command('apt-key finger') do
+  jitsi_apt_key = <<APT_KEY
+pub   4096R/2DC1389C 2016-06-23
+      Key fingerprint = 66A9 CD05 95D6 AFA2 4729  0D3B EF8B 479E 2DC1 389C
+uid                  Jitsi <dev@jitsi.org>
+sub   4096R/88D3172B 2016-06-23
+APT_KEY
+
+  # TODO: Perhaps this key still refers to unstable repo?
+  jitsi_apt_key_incorrect = <<APT_KEY_UNWANTED
+pub   1024D/EB0AB654 2008-06-20
+      Key fingerprint = 040F 5760 8F84 BAF1 BF84  4A62 C697 D823 EB0A B654
+uid                  SIP Communicator (Debian package) <deb-pkg@sip-communicator.org>
+sub   2048g/F6EFCE13 2008-06-20
+APT_KEY_UNWANTED
+  its('stdout') { should include(jitsi_apt_key) }
+  its('stdout') { should_not include(jitsi_apt_key_incorrect) }
+end
